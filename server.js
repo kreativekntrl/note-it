6f8013467bae09100348d5e3bba04677a0109cec
Ava <avamsteimle@gmail.com>
2022-01-30 01:38:13 -0800

    commiting changes to server.js:

--- server.js
+++ server.js
@@ -14,10 +14,31 @@ app.use(express.static("public"));
 
 // GET Route for getting notes
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-//GET route to send all non-included routes to landing page 
+//GET route for getting notes from our database
+app.get("/api/notes", (req, res) => 
+    res.json("/db/db.json")
+);
+
+//POST Route for new notes
+app.post("/api/notes", (req, res) => {
+    const { title, text, id } = req.body;
+
+    if (res.body) {
+        const newNote = {
+            title,
+            text,
+            id,
+        };
+
+        readAndAppend(newNote, "/db/notes.json");
+        res.json(`Note added successfully`);
+    } 
+});
+
+//GET route to send all non-specified routes to landing page 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 ); 
