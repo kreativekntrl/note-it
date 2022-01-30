4132c9d6cb0a26a54353cb0f3076530b458abd34
Ava <avamsteimle@gmail.com>
2022-01-29 18:43:57 -0800

    setting up server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+//Sends all non-included routes to index.html
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`)); 
