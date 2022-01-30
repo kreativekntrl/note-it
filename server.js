eee38fd3351b2d274fdcb44191da4a0c4c8c7788
Ava <avamsteimle@gmail.com>
2022-01-29 18:56:34 -0800

    added GET route for landingpage and /notes

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//Middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-//Sends all non-included routes to index.html
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+// GET Route for getting notes
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+//GET route to send all non-included routes to landing page 
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+); 
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`)); 
